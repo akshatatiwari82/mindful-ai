2e7196b8484efdf77e572e45be43e0354c7e1ac4
akshatatiwari82 <akshatatiwari82@gmail.com>
2025-12-31 22:48:22 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import Navbar from "./components/Navbar";
 import MoodTracker from "./components/MoodTracker";
 import Exercises from "./components/Exercises";
 import AdminDashboard from "./components/AdminDashboard";
+// Import your new components here
+import Home from "./components/Home";
+import Chat from "./components/Chat";
+import Emergency from "./components/Emergency";
+import Therapist portal from "./components/TherapistPortal";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-gray-50 font-sans text-gray-900">
-        {/* Navbar stays at the top always */}
         <Navbar />
-
-        {/* Main Content Area */}
         <main className="py-8">
           <Routes>
-            {/* The "/" path means this is the FIRST page you see (Home) */}
-            <Route path="/" element={<MoodTracker />} />
-            
-            {/* These are the add-on pages */}
+            <Route path="/" element={<Home />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/mood-tracker" element={<MoodTracker />} />
             <Route path="/exercises" element={<Exercises />} />
+            <Route path="/emergency" element={<Emergency />} />
+            <Route path="/therapist" element={<TherapistPortal />} />
             <Route path="/admin" element={<AdminDashboard />} />
           </Routes>
         </main>
